18b5b01872a08250cc719c93161af42efa58e3c2
adoudi-mondher <adoudi.mondher@gmail.com>
2025-08-30 03:50:35 +0200

    DB User changed

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 // config/db.js
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("yc0vd_sheisthegoal", "root", "", {
+const sequelize = new Sequelize("yc0vd_sheisthegoal", "yc0vd_nassim", "#gTz%r0w5W@F63PP", {
   host: "yc0vd.myd.infomaniak.com",
   dialect: "mysql",
   logging: false
